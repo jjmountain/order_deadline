e71d73223454efb7ded9f2ddae73bcaacaffc7c1
jjmountain <devereuxjj@gmail.com>
2020-12-10 18:40:08 +0900

    logging ids from resource picker

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,48 @@
-import { Page } from "@shopify/polaris";
+import { Page, Layout, EmptyState  } from "@shopify/polaris";
 import { ResourcePicker } from '@shopify/app-bridge-react'
+import React, { useState } from 'react';
+import store from 'store-js';
 
-class Index extends React.Component {
-  state = {
-    open: false
+function Index() {
+  const [modal, setModal] = useState({ open: false })
+  const emptyState = !store.get('ids');
+
+  const handleSelection = (resources) => {
+    const idsFromResources = resources.selection.map((product) => product.id)
+    setModal({open: false})
+    store.set('ids', idsFromResources)
+    console.log('products ids here', store.get('ids'));
   }
-  render() {
+
     return (
       <Page
         title='Product Selector'
         primaryAction={{
           content: 'Select products',
-          onAction: () => this.setState({open: true})
+          onAction: () => setModal({open: true})
         }}
       >
         <ResourcePicker 
           resourceType='Product'
-          open={this.state.open}
-          onCancel={() => this.setState({open: false})}
-          onSelection={(resources) => this.handleSelection(resources)}
+          open={modal.open}
+          onCancel={() => setModal({open: false})}
+          onSelection={(resources) => handleSelection(resources)}
         />
+        <Layout>
+          <EmptyState
+            heading="Manage your inventory transfers"
+            action={{
+              content: 'Select Products',
+              onAction: () => setModal({ open: true })
+            }}
+            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg" 
+          >
+            <p>Select Products</p>
+          </EmptyState>
+        </Layout>
+
       </Page>
     )
-  }
-
-  handleSelection = (resources) => {
-    this.setState({open: false})
-    console.log(resources)
-  }
 }
 
 
